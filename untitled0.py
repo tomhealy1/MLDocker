7c545370c4a4b151469feed27898b7a2c3e7fb73
tomhealy1 <alfaone156@gmail.com>
2019-05-04 16:18:35 +0100

    Added iris code

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
