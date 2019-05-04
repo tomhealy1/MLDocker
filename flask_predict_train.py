9d5763081e7e41cf6023371c1cefb3767bcdbb55
tomhealy1 <alfaone156@gmail.com>
2019-05-04 16:27:33 +0100

    Renamed file

---
+++ flask_predict_train.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+#loading the dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+#Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.5)
+
+#Build the model
+clf =RandomForestClassifier(n_estimators=10)
+
+#Train the classifer
+clf.fit(X_train, y_train)
+
+#Predictions
+predicted = clf.predict(X_test)
+
+# Accuracy
+
+print(accuracy_score(predicted, y_test))
+
