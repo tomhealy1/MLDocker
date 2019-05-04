4899ba757e1fc49276e33e438cfb936fefa59c1d
tomhealy1 <alfaone156@gmail.com>
2019-05-04 16:17:09 +0100

    Added iris code

--- flask1.py
+++ flask1.py
@@ -1,2 +1,27 @@
 #Created on 4/5/2019
-from flask import Flask
+
+
+
+
+
+
+#import the package and request method
+from flask import Flask, request
+#app name =
+app = Flask(__name__)
+
+#the slash is the url
+@app.route('/hello_world', methods=['GET', 'POST'])
+def add():
+    #a=request.form["a"]
+    #b=request.form["b"]
+    #c=request.form["c"]
+    return "Hello World" #str( int(a) + int(b) + int(c))
+
+#ef predict(house_size, house_beds):
+ #   prediction =ml_model.predict(house_size, house_beds)
+  #  return prediction
+
+if __name__=='__main__':
+    app.run(port=7000)
+
